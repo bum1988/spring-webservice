ec040434934dc0d7fa8077bb797760c37fee7c2a
bum1988 <bum1988@naver.com>
2019-02-13 00:53:41 +0900

    2_게시판만들기 중간까지 완료

--- src/main/java/com/yoonbum/webservice/web/WebRestController.java
+++ src/main/java/com/yoonbum/webservice/web/WebRestController.java
@@ -1,6 +1,8 @@
 package com.yoonbum.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //@RestController는 @ResponseBody를 모든 메소드에서 적용
@@ -10,4 +12,5 @@ public class WebRestController {
     public String hello() {
         return "HelloWorld";
     }
+    
 }
